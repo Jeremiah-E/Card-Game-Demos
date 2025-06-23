4a215dcc3108243eb162aa95eb08d2bbf9db25a3
Jeremiah-E <jkevans2006@gmail.com>
2025-06-23 14:50:28 -0500

    Introduction of Face and Suite

--- demo/src/helper.rs
+++ demo/src/helper.rs
@@ -1,16 +1,54 @@
 pub struct Card {
-    face: String,
-    suite: String
+    face: Face,
+    suite: Suite
+}
+pub enum Suite {
+    Diamonds(), Spades(), Hearts(), Clubs()
+}
+pub enum Face {
+    Ace(), Two(), Three(), Four(), Five(), Six(), Seven(), Eight(), Nine(), Ten(), Jack(), Queen(), King()
 }
 
 impl Card {
-    pub fn new(face: &str, suite: &str) -> Card {
-        return Card {face: face.to_string(), suite: suite.to_string()};
+    pub fn new(face: Face, suite: Suite) -> Card {
+        return Card {face, suite};
     }
 }
 
 impl std::fmt::Display for Card {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Card::new(Face::Queen, Suite::Clubs) -> "Queen of Clubs"
         write!(f, "{} of {}", self.face, self.suite)
+    }
+}
+impl std::fmt::Display for Suite {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Suite::Diamonds() => "Diamonds",
+            Suite::Spades() => "Spades",
+            Suite::Hearts() => "Hearts",
+            Suite::Clubs() => "Clubs",
+        };
+        write!(f, "{str}")
+    }
+}
+impl std::fmt::Display for Face {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Face::Ace() => "Ace",
+            Face::Two() => "2",
+            Face::Three() => "3",
+            Face::Four() => "4",
+            Face::Five() => "5",
+            Face::Six() => "6",
+            Face::Seven() => "7",
+            Face::Eight() => "8",
+            Face::Nine() => "9",
+            Face::Ten() => "10",
+            Face::Jack() => "Jack",
+            Face::Queen() => "Queen",
+            Face::King() => "King",
+        };
+        write!(f, "{str}")
     }
 }
--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,6 +4,6 @@ use helper::*;
 fn main() {
     println!("Hello, world! This is a test demo for trying out some basic ideas!");
     println!("For example, we can initialize a card!");
-    let card = Card::new("Jack", "Diamonds");
+    let card = Card::new(Face::Jack(), Suite::Diamonds());
     println!("I just initialized the card \x1b[33m{card}\x1b[m!")
 }
